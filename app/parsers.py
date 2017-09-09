c6fb0989acffa08b7efc17df4b28d6abed452c98
AM <andrej.mudroch@hotmail.com>
2017-09-10 00:06:56 +0200

    New regexes, creation of dataframe.

--- app/parsers.py
+++ app/parsers.py
@@ -1,6 +1,7 @@
 from fuzzywuzzy import fuzz
 import numpy as np
 import re
+import pandas as pd
 from . import logger
 
 
@@ -11,7 +12,12 @@ class BaseParser:
         self.list_text = [{'line': line, 'parsed': False, 'to_delete': False}
                           for line in recognized_text.splitlines() if line]
         self.goods_text = None
-        self.goods_df = None
+        self.good_df = None
+        self.date = None
+        self.total_amount = None
+
+    def parse(self):
+        pass
 
     def get_goods_text(self):
         pass
@@ -25,14 +31,27 @@ class BaseParser:
     def get_total_amount(self):
         pass
 
-    def return_not_parsed(self):
-        pass
+    @property
+    def not_parsed(self):
+        return [x['line'] for x in self.goods_text if not x['parsed']]
+
+    @staticmethod
+    def get_numbers(s):
+        return float(s.replace(" ", "").replace(',', '.'))
 
 
 class TescoParser(BaseParser):
     shop_identifiers = ['TESCO STORES', 'CLUBCARD']
-    simple_goods_pattern = re.compile('^(\D[\d\w\sÀ-ÿ.,/]*)\s(\d{1,4})\s?[.,]?[\s\w%]*$', flags=re.M | re.I)
-    amount_goods_pattern = re.compile('^(.*)\n([\w,\s]+)[\*©r].*\s(\d{1,3}\s*[.,]\s*\d{2,3})[BR8\s]$', flags=re.M)
+    total_amount_pattern = re.compile('(\d{1,4}\s?[,.]\s?\d{2})')
+    simple_goods_pattern = re.compile('^(\D[\w\sÀ-ÿ.,/%"—]*[^,])\s(\d{1,4}\s?[.,]?\d{0,2})[\s\w%]*$', flags=re.M | re.I)
+    two_line_goods_pattern = re.compile('^(.*)\n([\w,\s]+)[\*©r].*\s(\d{1,3}\s*[.,]\s*\d{2,3})[BR8\s]$', flags=re.M)
+
+    def parse(self):
+        self.get_goods_text()
+        self.get_total_amount()
+        self.parse_goods()
+        for l in self.not_parsed:
+            print(l)
 
     def get_goods_text(self):
         goods_start = ['K.c.', 'Kč', 'Kc', 'K <:', 'lži', '!(Cš']
@@ -51,24 +70,25 @@ class TescoParser(BaseParser):
             end_list.append(max(map(lambda x: fuzz.token_set_ratio(line['line'], x), goods_end)))
         start = np.argmax(start_list)
         end = np.argmax(end_list)
-        self.goods_text = self.list_text[start + 1:end]
+        self.goods_text = self.list_text[start + 1:end + 1]
         self.goods_text = list(filter(lambda x: not x['to_delete'], self.goods_text))
         logger.debug(self.goods_text)
 
     def parse_goods(self):
+        goods_list = []
 
-        # Second pass for the two-row goods
+        # First pass for the two-row goods
         for idx, line in enumerate(self.goods_text):
             if idx >= len(self.goods_text) - 1:
                 break
             if line['parsed'] or self.goods_text[idx + 1]['parsed']:
                 continue
-            match = re.match(self.amount_goods_pattern, "{}\n{}".format(line['line'], self.goods_text[idx + 1]['line']))
+            match = re.match(self.two_line_goods_pattern, "{}\n{}".format(line['line'], self.goods_text[idx + 1]['line']))
             if match is not None:
                 item, amount, price = match.groups()
                 line['parsed'] = True
                 self.goods_text[idx + 1]['parsed'] = True
-                logger.debug((item, amount, price))
+                goods_list.append({'Item': item, 'Amount': amount, 'Price': self.get_numbers(price)})
 
         for line in self.goods_text:
             if line['parsed']:
@@ -77,7 +97,16 @@ class TescoParser(BaseParser):
             if match is not None:
                 item, price = match.groups()
                 line['parsed'] = True
-                logger.debug((item, price))
+                goods_list.append({'Item': item, 'Amount': None, 'Price': self.get_numbers(price)})
+        df = pd.DataFrame(goods_list)
+        logger.debug("Goods generated %s", df)
+
+    def get_total_amount(self):
+        match = re.search(self.total_amount_pattern, self.goods_text[-1]['line'])
+        if match:
+            total_amount = match.group()
+            self.total_amount = self.get_numbers(total_amount)
+        self.goods_text.pop()
 
 
 class AlbertParser(BaseParser):
--- test.py
+++ test.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     im_path = ''
     text = app.ocr.extract_text_from_image(im_path)
     obj = app.ParserFactory.find_receipt_type(text)
-    obj.get_goods_text()
-    obj.parse_goods()
+    obj.parse()
